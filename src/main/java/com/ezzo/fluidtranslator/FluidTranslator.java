e61a47bb4e527f8632a7a95a024802d45ee083da
Bufka2011 <bufka.bulanov@gmail.com>
2026-01-15 14:11:40 -0700

    Versions

--- src/main/java/com/ezzo/fluidtranslator/FluidTranslator.java
+++ src/main/java/com/ezzo/fluidtranslator/FluidTranslator.java
@@ -27,7 +27,7 @@ import cpw.mods.fml.common.registry.GameRegistry;
 public class FluidTranslator {
 
     public static final String MODID = "bobfluidtranslator";
-    public static final String VERSION = "1.0.0";
+    public static final String VERSION = "1.1.0";
 
     @Mod.Instance(FluidTranslator.MODID)
     public static FluidTranslator instance;
--- src/main/java/com/ezzo/fluidtranslator/tileentity/TileEntityUniversalTank.java
+++ src/main/java/com/ezzo/fluidtranslator/tileentity/TileEntityUniversalTank.java
@@ -13,7 +13,12 @@ import net.minecraft.network.NetworkManager;
 import net.minecraft.network.Packet;
 import net.minecraft.network.play.server.S35PacketUpdateTileEntity;
 import net.minecraftforge.common.util.ForgeDirection;
-import net.minecraftforge.fluids.*;
+import net.minecraftforge.fluids.Fluid;
+import net.minecraftforge.fluids.FluidContainerRegistry;
+import net.minecraftforge.fluids.FluidStack;
+import net.minecraftforge.fluids.FluidTankInfo;
+import net.minecraftforge.fluids.IFluidContainerItem;
+import net.minecraftforge.fluids.IFluidHandler;
 
 import com.ezzo.fluidtranslator.ModFluidRegistry;
 import com.ezzo.fluidtranslator.TankModes;
